a4e1808d5433f59c9e989841028a77dd3e3c056e
Yasas Banuka <yasasbanukaofficial@gmail.com>
2025-10-28 18:34:46 +0530

    feat(dto): added VoteDTO

--- src/main/java/lk/ijse/votingsys/dto/VoteDTO.java
+++ src/main/java/lk/ijse/votingsys/dto/VoteDTO.java
@@ -1,4 +1,31 @@
 package lk.ijse.votingsys.dto;
 
-public class VoteDTO {
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class VoteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String option;
+    private int optionACount;
+    private int optionBCount;
+    private int optionCCount;
+
+    public VoteDTO(String username, String option) {
+        this.username = username;
+        this.option = option;
+    }
+
+    public void addCount(String option){
+        switch (option){
+            case "A" -> optionACount++;
+            case "B" -> optionBCount++;
+            case "C" -> optionCCount++;
+        }
+    }
 }
