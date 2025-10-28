80b99c0424de942b4c87449b0d57f8608b4b486a
Yasas Banuka <yasasbanukaofficial@gmail.com>
2025-10-28 21:39:15 +0530

    feat(controller): display vote when client starts

--- src/main/java/lk/ijse/votingsys/controller/ClientController.java
+++ src/main/java/lk/ijse/votingsys/controller/ClientController.java
@@ -1,5 +1,8 @@
 package lk.ijse.votingsys.controller;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
@@ -7,6 +10,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import lk.ijse.votingsys.dto.VoteDTO;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -26,7 +30,8 @@ public class ClientController implements Initializable {
     private Socket clientSocket;
     private ObjectInputStream objectIS;
     private ObjectOutputStream objectOS;
-    private final boolean isConnected = true;
+    private VoteDTO voteDTO = new VoteDTO();
+    private boolean isConnected = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -35,17 +40,47 @@ public class ClientController implements Initializable {
 
     private void connectToServer() {
         new Thread(() -> {
-            while(isConnected) {
-                try {
-                    if (clientSocket == null || clientSocket.isClosed()) {
-                        clientSocket = new Socket("localhost", 3000);
-                        objectIS = new ObjectInputStream(clientSocket.getInputStream());
-                        objectOS = new ObjectOutputStream(clientSocket.getOutputStream());
-                        statuslbl.setText("Connected to Server");
+            try {
+                clientSocket = new Socket("localhost", 3000);
+                objectOS = new ObjectOutputStream(clientSocket.getOutputStream());
+                objectIS = new ObjectInputStream(clientSocket.getInputStream());
+                Platform.runLater(() -> statuslbl.setText("Connected to Server"));
+
+                isConnected = true;
+                this.voteDTO = (VoteDTO) objectIS.readObject();
+                displayCount();
+                new Thread(() -> handleMessages(clientSocket)).start();
+            } catch (Exception e) {
+                Platform.runLater(() -> statuslbl.setText("Cannot Connect to Server"));
+                isConnected = false;
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void displayCount() {
+        Platform.runLater(() -> {
+            ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                    new PieChart.Data("Option A", voteDTO.getOptionACount()),
+                    new PieChart.Data("Option B", voteDTO.getOptionBCount()),
+                    new PieChart.Data("Option C", voteDTO.getOptionCCount())
+            );
+            voteChart.setData(pieChartData);
+        });
+    }
+
+    private void handleMessages(Socket clientSocket) {
+        new Thread(() -> {
+            try {
+                while (isConnected && clientSocket.isConnected()) {
+                    VoteDTO receivedDTO = (VoteDTO) objectIS.readObject();
+                    if (voteDTO != null) {
+                        this.voteDTO = receivedDTO;
+                        displayCount();
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }).start();
     }
--- src/main/java/lk/ijse/votingsys/controller/ServerController.java
+++ src/main/java/lk/ijse/votingsys/controller/ServerController.java
@@ -1,33 +1,68 @@
 package lk.ijse.votingsys.controller;
 
+import lk.ijse.votingsys.dto.VoteDTO;
+
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ServerController {
+    private final CopyOnWriteArrayList<ObjectOutputStream> clientOutputStreams = new CopyOnWriteArrayList<>();
     private Socket clientSocket;
     private ObjectInputStream objectIS;
     private ObjectOutputStream objectOS;
-    private final boolean isConnected = true;
+    private VoteDTO voteDto = new VoteDTO();
+    private boolean isConnected = true;
 
     public void startConnection() {
         new Thread(() -> {
-            while (isConnected) {
-                try (ServerSocket serverSocket = new ServerSocket(3000)){
-                    System.out.println("Server Started");
-                    System.out.println("Waiting for Client on port: 3000");
+            try (ServerSocket serverSocket = new ServerSocket(3000)){
+                System.out.println("Server Started");
+                System.out.println("Waiting for Client on port: 3000");
 
-                    clientSocket = serverSocket.accept();
-                    System.out.println("Client Connected");
+                clientSocket = serverSocket.accept();
 
-                    objectIS = new ObjectInputStream(clientSocket.getInputStream());
-                    objectOS = new ObjectOutputStream(clientSocket.getOutputStream());
-                } catch (Exception e) {
-                    System.out.println("Client Disconnected");
-                    e.printStackTrace();
-                }
+                objectIS = new ObjectInputStream(clientSocket.getInputStream());
+                objectOS = new ObjectOutputStream(clientSocket.getOutputStream());
+
+                clientOutputStreams.add(objectOS);
+                System.out.println("Client Connected");
+
+                broadcastVotes();
+                handleClient(clientSocket);
+            } catch (Exception e) {
+                System.out.println("Client Disconnected");
+                e.printStackTrace();
             }
         }).start();
+    }
+
+    private void handleClient(Socket clientSocket) {
+        new Thread(() -> {
+            try {
+                while (isConnected) {
+                    if (clientSocket != null && clientSocket.isConnected()) {
+                        this.voteDto = (VoteDTO) objectIS.readObject();
+                    }
+                }
+            } catch (Exception e) {
+                isConnected = false;
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void broadcastVotes(){
+        for (ObjectOutputStream cos : clientOutputStreams) {
+            try {
+                cos.reset();
+                cos.writeObject(voteDto);
+                cos.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
